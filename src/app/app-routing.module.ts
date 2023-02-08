027f841a5cec261c38e7f88a2abc2c51f808f288
EriikSilva <eriklipe@gmail.com>
2023-02-08 06:38:34 -0400

    fix - protegendo rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,8 @@ const routes: Routes = [
   },
   {
     path:"criar-conta",
-    component:SignInComponent
+    component:SignInComponent,
+    canActivate:[IsLoggeddGuard]
   },
   {
     path:'teste',
